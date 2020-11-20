ce74b870fbac2522ce593cd947a6a92f5ea4307d
eve-pornpawee <73640163+eve-pornpawee@users.noreply.github.com>
2020-11-20 09:28:05 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,38 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
 <html>
-    <head>
-        <title>game3</title>
+<head>
+    <little>Comment From</little>
+    <style>
+    body{
+    background-color:aquamarine;
+    }
+</style>
     </head>
-    <body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'total2.mysql.database.azure.com', 'cake@total2', 'Eve31521', 'game3', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM book');
-?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">link </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysqli_close($conn);
-?>
+<body>
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name" /><br>
+Age: <input type="text" name="age" /><br>
+<input type="submit" />
+</form>
 </body>
 </html>
